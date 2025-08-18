a91f7ad4bec8119b95f02fbd19359891dc36b6b9
dawei <tb6147877@hotmail.com>
2025-08-18 01:08:16 +0100

    add code

--- framework/layers/core.py
+++ framework/layers/core.py
@@ -1,8 +1,18 @@
 from framework.layers.base import Layer
-
+from framework.utils.initializations import _zero
+from framework.utils.initializations import get as get_init
 
 class Linear(Layer):
     def __init__(self, n_out, n_in=None, init='glorot_uniform'):
         self.n_out = n_out
         self.n_in = n_in
         self.out_shape = (None, n_out)
+        self.init = get_init(init)
+
+        self.W = None
+        self.b = None
+        self.dW=None
+        self.db=None
+        self.last_input=None
+
+
--- framework/utils/initializations.py
+++ framework/utils/initializations.py
@@ -38,8 +38,46 @@ class Normal(Initializer):
     def call(self,size):
         return _cast_dtype(get_rng().normal(loc=self.mean,scale=self.std,size=size))
 
+class GlorotUniform(Initializer):
+    def call(self,size):
+        fan_in,fan_out=decompose_size(size)
+        return Uniform(np.sqrt(6/(fan_in+fan_out)))(size)
+
 def _cast_dtype(res):
     return np.array(res, dtype=get_dtype())
 
+_zero=Zero()
+_one=One()
+
 #这个函数其实就在计算各种初始化算法的缩放
 def decompose_size(size):
+    if len(size)==2: #全连接层
+        fan_in=size[0]
+        fan_out=size[1]
+    elif len(size)==4 or len(size)==5: #卷积层
+        respective_field_size=np.prod(size[2:])
+        fan_in = size[1]*respective_field_size
+        fan_out=size[0]*respective_field_size
+    else:
+        fan_in=fan_out=int(np.sqrt(np.prod(size)))
+
+    return fan_in,fan_out
+
+
+def get(initialization):
+    if initialization.__class__.__name__=="str":
+        if initialization in ['zero','Zero']:
+            return Zero()
+        if initialization in ['one','One']:
+            return One()
+        if initialization in ['uniform','Uniform']:
+            return Uniform()
+        if initialization in ['normal','Normal']:
+            return Normal()
+        if initialization in ['glorot_uniform','GlorotUniform']:
+            return GlorotUniform()
+        raise ValueError('Unknown initialization name: {}.'.format(initialization))
+    elif isinstance(initialization, Initializer):
+        return copy.deepcopy(initialization)
+    else:
+        raise ValueError('Unknown type: {}.'.format(initialization.__class__.__name__))
